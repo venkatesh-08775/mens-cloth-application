26774a079b7e93e591925aea953fed51346a55cc
venkatesh-08775 <125874744+venkatesh-08775@users.noreply.github.com>
2025-04-26 15:09:06 +0530

    backend initialize

---
+++ Server/mens_cloth/mens_cloth/src/main/java/com/mens/cloth/mens_cloth/MensClothApplication.java
@@ -0,0 +1,13 @@
+package com.mens.cloth.mens_cloth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensClothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensClothApplication.class, args);
+	}
+
+}
---
+++ Server/mens_cloth/mens_cloth/src/test/java/com/mens/cloth/mens_cloth/MensClothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mens.cloth.mens_cloth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensClothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
